74440efa0019c861d466f40bc2697a94306868f2
Polkovnik-Daniil <0205danik@mail.ru>
2023-10-12 14:31:59 +0300

    Mission complete!

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,17 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public OrderService(ApplicationDbContext applicationDbContext) =>
+            _applicationDbContext = applicationDbContext;
+        public async Task<Order> GetOrder() =>
+            _applicationDbContext.Orders.OrderByDescending(order => order.Price).First();
+        public Task<List<Order>> GetOrders() =>
+            Task.FromResult(_applicationDbContext.Orders.Where(order => order.Quantity > 10).ToList());
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public UserService(ApplicationDbContext applicationDbContext) => 
+            _applicationDbContext = applicationDbContext;
+
+        public Task<User> GetUser() =>
+            Task.FromResult(_applicationDbContext.Users.OrderByDescending(user => user.Orders.Count).First());
+
+        public Task<List<User>> GetUsers() =>
+            Task.FromResult(_applicationDbContext.Users.Where(user => user.Status == Enums.UserStatus.Inactive).ToList());
+    }
+}
